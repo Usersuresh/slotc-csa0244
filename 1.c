23c8384061b51782938e7fbdf43fa076b0318b92
Usersuresh <112493936+Usersuresh@users.noreply.github.com>
2022-09-27 11:27:47 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,35 +1,25 @@
+
 #include <stdio.h>
 
-int main()
+int main() {
+
+  int n, reverse = 0, remainder;
+
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+ If(n>0) 
 {
-    int i, num, sum = 0;
+while (n != 0) {
+    remainder = n % 10;
+    reverse = reverse * 10 + remainder;
+    n /= 10;
+  }
 
-    /* Input a number from user */
-    printf("Enter any number to check perfect number: ");
-    if(scanf("%d", &num)==1){
-    	 /* Calculate sum of all proper divisors */
-    for(i = 1; i <= num / 2; i++)
-    {
-        /* If i is a divisor of num */
-        if(num%i == 0)
-        {
-            sum += i;
-        }
-    }
+  printf("Reversed number = %d", reverse);
+}
+else
+Printf(" Invalid") 
 
-    /* Check whether the sum of proper divisors is equal to num */
-    if(sum == num && num > 0)
-    {
-        printf("%d is PERFECT NUMBER", num);
-    }
-    else
-    {
-        printf("%d is NOT PERFECT NUMBER", num);
-    }
-    	
-	}
-	else
-	printf("invalid");
 
-    return 0;
+  return 0;
 }
