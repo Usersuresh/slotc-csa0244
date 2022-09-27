1d2637cea813a46e06e9cbe29bebc37a774bfa0d
Usersuresh <112493936+Usersuresh@users.noreply.github.com>
2022-09-27 06:31:34 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
-#include <string.h> 
-void swap (char *x, char *y)
-{
-    char temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-  }
-void permute(char *a, int i, int n)
-{
-    int j;
-    if (i == n)
-        printf("%s\n", a);
-    else {
-        for (j = i; j <= n; j++)
-        {
-            swap((a + i), (a + j));
-            permute(a, i + 1, n);
-            swap((a + i), (a + j)); //backtrack
-        }
-    }
+#include <string.h>
+void swap(char *x, char *y){
+char temp;
+temp = *x;
+*x = *y;
+*y = temp;
 }
-
-int main()
-{
-    char a[20];
-    int n;
-    printf("Enter a string: ");
-    scanf("%s", a);
-    n = strlen(a);
-    printf("Permutaions:\n"); 
-    permute(a, 0, n - 1);
-    getchar();
-    return 0;
+void permute(char *a, int l, int r){
+int i;
+if (l == r)
+printf("%s\n", a);
+else{
+for (i = l; i <= r; i++){
+swap((a+l), (a+i));
+permute(a, l+1, r);
+swap((a+l), (a+i));
+}
+}
+}
+int main(){
+char str[10];
+int i;
+printf("Enter The String : ");
+scanf("%s",str);
+i = 0;
+if(str[i] == '-'){
+printf("INVALID INPUT");
+i++;
+}
+else{
+int n = strlen(str);
+permute(str, 0, n-1);
+}
+return 0;
 }
